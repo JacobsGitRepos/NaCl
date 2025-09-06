04f3fde0987971499c8f9c9ea867d2fe4e32352d
JW <148696634+JacobsGitRepos@users.noreply.github.com>
2025-09-06 23:15:24 +0100

    Add main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,1 @@
-import pygame
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-pygame.quit()
+# main.py
